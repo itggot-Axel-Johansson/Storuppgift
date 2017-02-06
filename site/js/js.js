f745194dc4965a2a1767d5116ec56be34aa6dfd7
itggot-Axel-Johansson <axel.johansson@itggot.se>
2017-02-06 15:02:00 +0100

    massa hrejor

--- site/js/js.js
+++ site/js/js.js
@@ -1,3 +1,43 @@
 /**
  * Created by axel.johansson2 on 2017-02-03.
  */
+/**
+ * Created by axel.johansson2 on 2017-01-23.
+ */
+function hello() {
+    console.log("Hello, World");
+}
+
+function changeColor(grillkorv) {
+    grillkorv.classList.toggle('red');
+}
+function reset() {
+    var elements = document.querySelectorAll('.red')
+    elements.forEach(function (element) {
+        element.classList.remove('red');
+    });
+}
+/* Här kan du göra så du tar bort en class
+ function changeColor2(grillkorv) {
+ grillkorv.classList.toggle('innan');
+ }*/
+
+function remove1() {
+    var elements = document.querySelectorAll('.box')
+    elements.forEach(function (element) {
+        element.classList.remove('box');
+    });
+}
+function remove() {
+    var elements = document.querySelectorAll('.innan')
+    elements.forEach(function (element) {
+        element.classList.remove('innan');
+    });
+}
+
+function add() {
+    var elements = document.querySelectorAll('.innan')
+    elements.forEach(function (element) {
+        element.classList.toggle('efter');
+    });
+}
